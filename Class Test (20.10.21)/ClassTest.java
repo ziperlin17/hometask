6f53261d32f988c68b2243aeb77f96fc11c30287
Данил Соловьев <89980506+ziperlin17@users.noreply.github.com>
2021-10-19 22:17:50 +0300

    Add files via upload

---
+++ Class Test (20.10.21)/ClassTest.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class ClassTest {
+    public static void main(String[] args) {
+        System.out.println("Найти кабинет? (Y/N)" );
+        Scanner scanner = new Scanner(System.in);
+        String Answer = scanner.next();
+        String yes = "Y";
+        String no = "N";
+        if (Answer.equals(yes)) {
+            double numberRandom = (int) 101 + Math.random()*1699;
+            Integer numberRandomTry = (int)numberRandom;
+            double amountRandom = (int) 1 + Math.random()*30;
+            Integer amountRandomTry = (int)amountRandom;
+            boolean lessonRandom;
+            if (amountRandom > 2) {
+                lessonRandom = true;
+            } else {
+                lessonRandom = false;
+            }
+            ClassRoom classRandom = new ClassRoom(numberRandomTry, amountRandomTry, lessonRandom);
+            System.out.println("-----------------------------");
+            System.out.println("Аудитория №"+classRandom.number);
+            System.out.println("Там "+classRandom.amount+" человек");
+            if (classRandom.lesson == true) {
+                System.out.println("Идет занятие");
+            } else {
+                System.out.println("Занятия сейчас нет");
+            }
+            System.out.println("-----------------------------");
+            System.out.println("\n"+"-Зайду в кабинет");
+            classRandom.ask();
+            System.out.println("-"+classRandom.boyNextDoor());
+        } else {
+            ClassRoom definiteClass = new ClassRoom(1309,27,true);
+            OurClassRoom OurClass = new OurClassRoom(true);
+            ClassRoom.description();
+            System.out.println("| "+definiteClass.number+"          "+definiteClass.amount+"                   "+ definiteClass.lesson+"          |");
+            System.out.println("Зайду в кабинет");
+            definiteClass.ask();
+            System.out.println("-"+definiteClass.boyNextDoor());
+            System.out.println("");
+            System.out.println("-Какое у вас расписание?");
+            OurClass.classSchedule();
+            System.out.println("-Список студентов?");
+            OurClass.studentsData();
+        }
+    }
+}
---
+++ Class Test (20.10.21)/OurClassRoom.java
@@ -0,0 +1,12 @@
+public class OurClassRoom {
+    boolean needSomething;
+    public OurClassRoom(boolean needSomething){
+        this.needSomething=needSomething;
+    }
+    void classSchedule() {
+        System.out.println("https://docs.google.com/spreadsheets/d/1bm6fgU2Z8A1xrnpxIMG2WI4lNv-nPlBAL4apVzxx6yA/edit#gid=709015033");
+    }
+    void studentsData() {
+        System.out.println("https://docs.google.com/spreadsheets/d/1bm6fgU2Z8A1xrnpxIMG2WI4lNv-nPlBAL4apVzxx6yA/edit#gid=0");
+    }
+}
